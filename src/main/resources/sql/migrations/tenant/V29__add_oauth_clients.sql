95072c599c251a50e4dcc447457373226425aa69
Zoltan Nebli <zoltan.nebli@dpc.hu>
2020-07-13 15:48:26 +0200

    give all access to channel clients

--- src/main/resources/sql/migrations/tenant/V29__add_oauth_clients.sql
+++ src/main/resources/sql/migrations/tenant/V29__add_oauth_clients.sql
@@ -1,2 +1,2 @@
-INSERT INTO `oauth_client_details` (`client_id`, `client_secret`, `resource_ids`, `scope`, `authorized_grant_types`, `access_token_validity`)
-VALUES ('channel-${tenantDatabase}', '${channelClientSecret}', '${identityProviderResourceId},${tenantDatabase}', 'identity', 'client_credentials', ${clientAccessTokenValidity});
+INSERT INTO `oauth_client_details` (`client_id`, `client_secret`, `resource_ids`, `scope`, `authorized_grant_types`, `access_token_validity`, `authorities`)
+VALUES ('channel-${tenantDatabase}', '${channelClientSecret}', '${identityProviderResourceId},${tenantDatabase}', 'identity', 'client_credentials', ${clientAccessTokenValidity}, 'ALL_FUNCTIONS');
