2e0011ee2e31a6274d6095e32b575801d3a6b194
Ashutosh Sharma <ashutoshsharma0061@gmail.com>
2020-08-10 14:16:32 +0530

    feat: pretty print json parameter
    modified pipe to print json parameter in pretty format.

--- src/app/pipes/pretty-print.pipe.ts
+++ src/app/pipes/pretty-print.pipe.ts
@@ -6,13 +6,59 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class PrettyPrintPipe implements PipeTransform {
 
   transform(val: any) {
-    return JSON.stringify(val, undefined, 4)
-      .replace(/\\\\n|\\\\r|\\\\t/g, '<br/>')
-      .replace(/\\\\\\"/g, '"')
-      .replace(/\\\"/g, '"')
-      .replace(/\\\"/g, '"')
-      .replace(/ /g, '\u00a0')
-      .replace(/\"/g, '');
+    var s="";
+    var tb=0;
+    val=String(val);
+    console.log(val.length);
+    var i=0;
+    for(i=0;i<val.length;i++) {
+      //console.log(i);
+      if(val[i] ==='}') {
+        tb=tb-1;
+        for(var j=0;j<tb;j++) {
+          s+="&nbsp;&nbsp;&nbsp;&nbsp;";
+        }
+        s+=val[i];
+        if(i<val.length && val[i+1]===',') {
+          s+=val[i+1];
+          i++;
+        }
+        s+='<br/>';
+        continue;
+      }
+      for(var j=0;j<tb;j++) {
+        s+="&nbsp;&nbsp;&nbsp;&nbsp;";
+      }
+      
+        var j=i;
+        while(j<val.length) {
+          if(val[j]!='\\' && val[j]!='"') {
+            if(val[j]===',') {
+              s+=val[j];
+              s+='<br/>';
+              j++;
+              break;
+            }
+            else if(val[j]==='{') {
+              s+=val[j];
+              s+='<br/>';
+              tb++;
+              j++;
+              break;
+            }
+            else if(val[j]==='}') {
+              s+='<br/>';
+              break;
+            }
+            else
+              s+=val[j];
+          }
+          j++;
+        } 
+        i=j-1;
+      
+    } 
+    return s;   
   }
 
 }
