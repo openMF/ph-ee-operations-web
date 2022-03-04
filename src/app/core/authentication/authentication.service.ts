e798d025e5c93999f520bc08693da4028ffc1d32
Chiranjib <chiranjiv@fynarfin.io>
2022-03-04 11:33:12 +0530

    Font-awesome

--- src/app/core/authentication/authentication.service.ts
+++ src/app/core/authentication/authentication.service.ts
@@ -117,7 +117,7 @@ export class AuthenticationService {
       if (environment.oauth.basicAuth === "true") {
         this.authorizationToken = `Basic ${environment.oauth.basicAuthToken}`;
       }
-      return this.http.post(`${environment.oauth.serverUrl}/oauth/token`, {}, { params: httpParams })
+      return this.http.disableApiPrefix().post(`${environment.oauth.serverUrl}/oauth/token`, {}, { params: httpParams })
         .pipe(
           map((tokenResponse: OAuth2Token) => {
             // TODO: fix UserDetails API
@@ -185,7 +185,7 @@ export class AuthenticationService {
         this.authorizationToken = `Basic ${environment.oauth.basicAuthToken}`;
       }
 
-      return this.http.post(`${environment.oauth.serverUrl}/oauth/token`, {}, { params: httpParams })
+      return this.http.disableApiPrefix().post(`${environment.oauth.serverUrl}/oauth/token`, {}, { params: httpParams })
       .pipe(map((tokenResponse: OAuth2Token) => {
         this.refreshAccessToken = false;
         this.storage.setItem(this.oAuthTokenDetailsStorageKey, JSON.stringify(tokenResponse));
