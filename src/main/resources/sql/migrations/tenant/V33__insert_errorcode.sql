6f66b7c32549b10cd4adfbde4d2b1470b66f36b1
Danish Jamal <danishjamal.104@gmail.com>
2022-03-08 19:26:23 +0530

    Removed extra spaces

--- src/main/resources/sql/migrations/tenant/V33__insert_errorcode.sql
+++ src/main/resources/sql/migrations/tenant/V33__insert_errorcode.sql
@@ -1,31 +1,31 @@
 INSERT INTO `errorcode` (`TRANSACTION_TYPE`, `ERROR_MESSAGE`, `ERROR_CODE`, `RECOVERABLE`)
 VALUES (
-           'COLLECTION-MPESA',
-           'The transaction is being processed',
-           '500.001.1001',
-           true
-       );
+    'COLLECTION-MPESA',
+    'The transaction is being processed',
+    '500.001.1001',
+    true
+);
 
 INSERT INTO `errorcode` (`TRANSACTION_TYPE`, `ERROR_MESSAGE`, `ERROR_CODE`, `RECOVERABLE`)
 VALUES (
-           'COLLECTION-MPESA',
-           'DS timeout.',
-           '1037',
-           true
-       );
+    'COLLECTION-MPESA',
+    'DS timeout.',
+    '1037',
+    true
+);
 
 INSERT INTO `errorcode` (`TRANSACTION_TYPE`, `ERROR_MESSAGE`, `ERROR_CODE`, `RECOVERABLE`)
 VALUES (
-           'COLLECTION-MPESA',
-           'Request cancelled by user',
-           '1031',
-           false
-       );
+    'COLLECTION-MPESA',
+    'Request cancelled by user',
+    '1031',
+    false
+);
 
 INSERT INTO `errorcode` (`TRANSACTION_TYPE`, `ERROR_MESSAGE`, `ERROR_CODE`, `RECOVERABLE`)
 VALUES (
-           'COLLECTION-MPESA',
-           'Request cancelled by user',
-           '1032',
-           false
-       );
+    'COLLECTION-MPESA',
+    'Request cancelled by user',
+    '1032',
+    false
+);
