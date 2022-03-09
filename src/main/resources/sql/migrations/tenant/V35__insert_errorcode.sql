eb51656646d7711e9a35f312689f5ae9eecc3285
Danish Jamal <danishjamal.104@gmail.com>
2022-03-09 11:55:38 +0530

    New recoverable error codes added

---
+++ src/main/resources/sql/migrations/tenant/V35__insert_errorcode.sql
@@ -0,0 +1,24 @@
+INSERT INTO `errorcode` (`TRANSACTION_TYPE`, `ERROR_MESSAGE`, `ERROR_CODE`, `RECOVERABLE`)
+VALUES (
+    'COLLECTION-MPESA',
+    '',
+    'BWENGINE-100029',
+    true
+);
+
+INSERT INTO `errorcode` (`TRANSACTION_TYPE`, `ERROR_MESSAGE`, `ERROR_CODE`, `RECOVERABLE`)
+VALUES (
+    'COLLECTION-MPESA',
+    '',
+    'BW-HTTP-100300',
+    true
+);
+
+
+INSERT INTO `errorcode` (`TRANSACTION_TYPE`, `ERROR_MESSAGE`, `ERROR_CODE`, `RECOVERABLE`)
+VALUES (
+    'COLLECTION-MPESA',
+    'System busy. The service request is rejected.',
+    '26',
+    true
+);
