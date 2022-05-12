21adb91386102e63305d4b4935614d9605bb2e42
Chiranjib <chiranjiv@fynarfin.io>
2022-05-12 13:51:57 +0530

    Updated status dropdown

---
+++ src/app/payment-hub/request-to-pay/helper/incoming-reqest.helper.ts
@@ -0,0 +1,22 @@
+export const transactionStatusData = [
+    {
+        option: 'ACCEPTED',
+        value: 'ACCEPTED',
+        css: 'green'
+    },
+    {
+        option: 'Failed',
+        value: 'FAILED',
+        css: 'red'
+    },
+    {
+        option: 'In progress',
+        value: 'IN_PROGRESS',
+        css: 'orange'
+    },
+    {
+        option: '',
+        value: 'UNKNOWN',
+        hidden: true
+    }
+];
