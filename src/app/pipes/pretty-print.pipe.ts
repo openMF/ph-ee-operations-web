5a11ec63b6171c56b0bf494a97145f1c088c4c45
Kristof Jozsa <kristof.jozsa@gmail.com>
2023-05-24 16:50:40 +0200

    format beautified xml/json content

--- src/app/pipes/pretty-print.pipe.ts
+++ src/app/pipes/pretty-print.pipe.ts
@@ -8,13 +8,13 @@ export class PrettyPrintPipe implements PipeTransform {
 
     transform(value: any) {
         if (value.charAt(0) === '{' && value.charAt(value.length - 1) === '}') {
-            return JSON.stringify(value, null, 2)
-                .replace(' ', '&nbsp;')
-                .replace('\n', '<br/>');
+            return vkbeautify.json(value);
         }
 
         if (value.charAt(0) === '<' && value.charAt(value.length - 1) === '>') {
-            return vkbeautify.xml(value);
+            return '<textarea>'
+                + vkbeautify.xml(unescape(value), 40)
+                + '</textarea>';
         }
 
         return value;
