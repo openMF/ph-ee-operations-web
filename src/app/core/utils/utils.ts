8821d0a31fb94a92cf191ffb2f2cfb50118b776b
Kelvin Thuku <kelvinthuku@Jack-Horsey.local>
2023-08-25 13:30:27 +0300

    code cleanup

--- src/app/core/utils/utils.ts
+++ src/app/core/utils/utils.ts
@@ -4,10 +4,10 @@ import { Injectable } from '@angular/core';
     providedIn: 'root'
 })
 export class Utils {
-    parseJwtToken(token: String) {
-        var base64Url = token.split('.')[1];
-        var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-        var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function (c) {
+    parseJwtToken(token: string) {
+        let base64Url = token.split('.')[1];
+        let base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+        let jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function (c) {
             return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
         }).join(''));
 
