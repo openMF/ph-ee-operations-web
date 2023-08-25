2698068985a44e1599c4af6a42f965c645ffc55c
Kelvin Thuku <kelvinthuku@Jack-Horsey.local>
2023-08-25 13:16:52 +0300

    sidebar check for permissions to show/hide admin menu

---
+++ src/app/core/utils/utils.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class Utils {
+    parseJwtToken(token: String) {
+        var base64Url = token.split('.')[1];
+        var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+        var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function (c) {
+            return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+        }).join(''));
+
+        return JSON.parse(jsonPayload);
+    }
+}
