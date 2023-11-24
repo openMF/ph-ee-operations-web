f01a4c96719fa83c8ddbd0a785eb9749a96e1e27
Dániel Gyenizse <daniel.gyenizse@binx.hu>
2023-11-24 10:25:03 +0100

    Removed time columns from transaction business attributes on transaction details screen

--- src/app/payment-hub/transactions/transaction-details.component.ts
+++ src/app/payment-hub/transactions/transaction-details.component.ts
@@ -52,7 +52,7 @@ export class TransactionDetailsComponent implements OnInit {
   /** Columns to be displayed in transaction table. */
   displayedColumns: string[] = ['timestamp', 'elementId', 'type', 'intent', 'actions'];
   displayedColumnsDetailsTable: string[] = ['timestamp', 'elementId', 'type', 'intent'];
-  displayedBusinessAttributeColumns: string[] = ['name', 'timestamp', 'value'];
+  displayedBusinessAttributeColumns: string[] = ['name', 'value'];
   /** Data source for transaction table. */
   taskList: MatTableDataSource<any>;
   businessAttributes: MatTableDataSource<any>;
