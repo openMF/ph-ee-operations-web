7152e3aca98fe193c5879ab2dc2fff9dd123e5d2
Gilbert Adjin Frimpong <gilclefes@gmail.com>
2024-09-08 10:14:12 +0000

    chore: upgrade @fortawesome/free-solid-svg-icons to version 6.4.2

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,22 +1,22 @@
 /** Angular Imports */
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { ReactiveFormsModule, FormsModule } from "@angular/forms";
 
 /** Custom Components */
-import { FormfieldComponent } from './form-dialog/formfield/formfield.component';
-import { FormDialogComponent } from './form-dialog/form-dialog.component';
-import { DeleteDialogComponent } from './delete-dialog/delete-dialog.component';
-import { FileUploadComponent } from './file-upload/file-upload.component';
-import { FooterComponent } from './footer/footer.component';
-import { LanguageSelectorComponent } from './language-selector/language-selector.component';
-import { ThemePickerComponent } from './theme-picker/theme-picker.component';
+import { FormfieldComponent } from "./form-dialog/formfield/formfield.component";
+import { FormDialogComponent } from "./form-dialog/form-dialog.component";
+import { DeleteDialogComponent } from "./delete-dialog/delete-dialog.component";
+import { FileUploadComponent } from "./file-upload/file-upload.component";
+import { FooterComponent } from "./footer/footer.component";
+import { LanguageSelectorComponent } from "./language-selector/language-selector.component";
+import { ThemePickerComponent } from "./theme-picker/theme-picker.component";
 
 /** Custom Modules */
-import { IconsModule } from './icons.module';
-import { MaterialModule } from './material.module';
-import { TranslateModule } from '@ngx-translate/core';
-import { ConfirmDialogComponent } from './confirm-dialog/confirm-dialog.component';
+import { IconsModule } from "./icons.module";
+import { MaterialModule } from "./material.module";
+import { TranslateModule } from "@ngx-translate/core";
+import { ConfirmDialogComponent } from "./confirm-dialog/confirm-dialog.component";
 
 /**
  * Shared Module
@@ -29,7 +29,7 @@ import { ConfirmDialogComponent } from './confirm-dialog/confirm-dialog.componen
     IconsModule,
     MaterialModule,
     ReactiveFormsModule,
-    TranslateModule.forRoot()
+    TranslateModule.forRoot(),
   ],
   declarations: [
     FormfieldComponent,
@@ -51,12 +51,7 @@ import { ConfirmDialogComponent } from './confirm-dialog/confirm-dialog.componen
     MaterialModule,
     FormsModule,
     ReactiveFormsModule,
-    TranslateModule
+    TranslateModule,
   ],
-  entryComponents: [
-    FormDialogComponent,
-    ConfirmDialogComponent,
-    DeleteDialogComponent
-  ]
 })
-export class SharedModule { }
+export class SharedModule {}
